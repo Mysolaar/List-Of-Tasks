bd7b92540a858cf8cd53ec993d56cfae04423f18
Magdalena Grądek <magdalena.gradek@gmail.com>
2023-06-12 21:09:24 +0200

    edit,complete, del btn

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ const addNewTodo = () => {
     if (todoInput.value !== '') {
         newTodo = document.createElement('li');
         newTodo.textContent = todoInput.value;
+        
+        createToolsArea()
         ulList.append(newTodo);
+        
         todoInput.value = '';
         errorInfo.textContent = '';
     } else {
@@ -28,6 +31,23 @@ const addNewTodo = () => {
   }
 }
 
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add("tools");
+    newTodo.append(toolsPanel)
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.textContent = 'EDIT';
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+    toolsPanel.append(completeBtn,editBtn,deleteBtn)
+}
 
 
 
